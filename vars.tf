22a764b9c0bdd6fd2e03d48bedfbc84a92f42bdd
chethan <chethangowda@gamil.com>
2023-01-11 17:35:24 +0530

    resolve the conflect

--- vars.tf
+++ vars.tf
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 variable "ami_id" {}
 
 
@@ -9,4 +10,16 @@ variable "instance_type" {
 
 variable "ec2_count" {
   default = "1"
+=======
+variable "vpc_cidr" {
+  default = "10.0.0.0/16"
+}
+variable "tenancy" {
+  default = "dedicated"
+}
+variable "vpc_id" {}
+
+variable "subnet_cidr" {
+  default = "10.0.1.0/24"
+>>>>>>> 6a9ded6 (vpc codes)
 }
